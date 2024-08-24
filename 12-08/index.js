c6f94c5b0c75011e2bfd7ae04da821bd030dba14
Julia Ayomide <145088749+juliaayomide@users.noreply.github.com>
2024-08-23 21:55:55 -0300

    Update index.js

--- 12-08/index.js
+++ 12-08/index.js
@@ -8,9 +8,11 @@ const app = express();
 const handlebars = exphbs.create({
     partialsDir: ["views/partials"]  // Diretório dos partials
 });
-
 app.engine('handlebars', handlebars.engine);
 app.set("view engine", "handlebars");
+
+// carregando noticias do JSON
+const noticias = JSON.parse(fs.readFileSync(paht.join(__dirname, 'noticias.json'),'utf8')).noticias;
 
 // Definindo o diretório de visualizações (views)
 app.set('views', path.resolve('views'));
@@ -21,19 +23,26 @@ app.use(
     })
 );
 
+// Rota de notícia
+app.get('/noticia/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const noticia = noticias.find(n => n.id === id);
+
+    if (noticia) {
+        res.render('noticia', noticia);
+    } else {
+        res.status(404).send('Notícia não encontrada');
+    }
+});
+
 app.use(express.json());
 
 app.use(express.static("public"));
 app.use(express.static("node_modules"));
 
-// Primeira rota
+// rota
 app.get('/', function (req, res) {
-    res.send('Hello World');
-});
-
-// Segunda rota
-app.get('/html', function (req, res) {
-    res.render('index'); // Renderiza o arquivo index.hbs
+    res.render('index', { noticias }); // Renderiza o arquivo index.hbs
 });
 
 app.listen(3000, () => {
